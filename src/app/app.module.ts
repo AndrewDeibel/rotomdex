961f5c45017fd96336cc2ea95ab4f522d6f2f5f9
Andrew Deibel <adeibel5@gmail.com>
2022-01-17 18:07:41 -0800

    PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -235,6 +235,8 @@ import {
   faStickyNote,
   faStar,
 } from '@fortawesome/pro-duotone-svg-icons';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -324,6 +326,12 @@ import {
     ColorPickerModule,
     WebcamModule,
     InlineSVGModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     {
