4355101fc4abc0e575b497316741f83e658936c0
Andrew Deibel <adeibel5@gmail.com>
2022-08-18 19:46:16 -0700

    removed app menu from footer/update footer links

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -14,56 +14,60 @@ export class FooterComponent implements OnInit {
 
   ngOnInit() {
     this.year = new Date().getFullYear();
-    this.menuLeft = new Menu({
-      items: [
-        new MenuItem({
-          icon: Icons.house,
-          text: 'Home',
-          route: '/',
-          exactMatch: true,
-        }),
-        new MenuItem({
-          icon: Icons.box,
-          text: 'Expansions',
-          route: '/expansions',
-        }),
-        new MenuItem({
-          symbol: Symbols.cards,
-          text: 'Cards',
-          route: '/cards',
-        }),
-        new MenuItem({
-          symbol: Symbols.pokeball,
-          text: 'Pokémon',
-          route: '/pokemon',
-        }),
-        new MenuItem({
-          icon: Icons.archive,
-          text: 'Collection',
-          route: '/collection',
-        }),
-      ],
-    });
+    // this.menuLeft = new Menu({
+    //   items: [
+    //     new MenuItem({
+    //       icon: Icons.house,
+    //       text: 'Home',
+    //       route: '/',
+    //       exactMatch: true,
+    //     }),
+    //     new MenuItem({
+    //       icon: Icons.box,
+    //       text: 'Expansions',
+    //       route: '/expansions',
+    //     }),
+    //     new MenuItem({
+    //       symbol: Symbols.cards,
+    //       text: 'Cards',
+    //       route: '/cards',
+    //     }),
+    //     new MenuItem({
+    //       symbol: Symbols.pokeball,
+    //       text: 'Pokémon',
+    //       route: '/pokemon',
+    //     }),
+    //     new MenuItem({
+    //       icon: Icons.archive,
+    //       text: 'Collection',
+    //       route: '/collection',
+    //     }),
+    //   ],
+    // });
     this.menuCenter = new Menu({
+      horizontal: true,
       items: [
         new MenuItem({
           href: 'https://discord.gg/FpPzMD2ZxP',
           text: 'Discord',
           symbol: Symbols.discord,
         }),
+        // new MenuItem({
+        //   text: 'Patreon',
+        //   symbol: Symbols.patreon,
+        // }),
         new MenuItem({
-          text: 'Patreon',
-          symbol: Symbols.patreon,
-        }),
-        new MenuItem({
+          href: 'https://www.youtube.com/channel/UCtaDnvCLnDQeJZjP66et-hQ',
           text: 'YouTube',
           symbol: Symbols.youtube,
         }),
         new MenuItem({
+          href: 'https://www.facebook.com/RotomdexSupport/',
           text: 'Facebook',
           symbol: Symbols.facebook,
         }),
         new MenuItem({
+          href: 'https://twitter.com/RotomdexSupport',
           text: 'Twitter',
           symbol: Symbols.twitter,
         }),
