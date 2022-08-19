75e53c3059f7620085fefe5ebd4806e2df82b97d
Andrew Deibel <adeibel5@gmail.com>
2022-08-18 22:40:12 -0700

    add to homescreen

--- src/app/controls/hero/hero.component.ts
+++ src/app/controls/hero/hero.component.ts
@@ -1,6 +1,8 @@
+import { Button } from './../button/button';
 import { AuthenticationService } from '@app/pages/auth/auth.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
+import { Icons } from '@app/models';
 
 @AutoUnsubscribe()
 @Component({
@@ -10,10 +12,41 @@ import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
 })
 export class HeroComponent implements OnInit {
   message: string;
+  installButton: Button;
+
+  deferredPrompt: any;
+  showButton = false;
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onbeforeinstallprompt(e: any) {
+    console.log(e);
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    this.deferredPrompt = e;
+    this.showButton = true;
+  }
+  addToHomeScreen() {
+    // Show the prompt
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice.then((choiceResult: any) => {
+      // if (choiceResult.outcome === 'accepted') {
+      //   console.log('User accepted the A2HS prompt');
+      // } else {
+      //   console.log('User dismissed the A2HS prompt');
+      // }
+      this.deferredPrompt = null;
+    });
+  }
 
   constructor(private authenticationService: AuthenticationService) {}
 
   ngOnInit() {
+    this.installButton = new Button({
+      text: 'Download App',
+      icon: Icons.download,
+      click: () => this.addToHomeScreen(),
+    });
     this.authenticationService.currentUserObservable().subscribe((user) => {
       if (user)
         this.message = `Hello ${user.name}, welcome to Rotomdex! Rotomdex is a Pokemon card management app.`;
